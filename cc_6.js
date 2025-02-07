772e3c902b627909ebc489bb2cd091da8735f2c6
mallikanicole <malmichael23@gmail.com>
2025-02-07 18:49:04 -0500

    Task 1 - Business Profit Calculation

--- cc_6.js
+++ cc_6.js
@@ -1,1 +1,9 @@
-//
+//Task 1-Function Declaration
+
+let calculateProfit=function
+profit(costPrice, sellingPrice, unitsSold){
+    return ((sellingPrice-costPrice)*unitsSold);
+};//create profit function
+
+console.log(`Profit:$${calculateProfit(20, 30, 100)}`); // Expected output: "Total Profit: $1000"
+console.log(`Profit:$${calculateProfit(50, 70, 200)}`); // Expected output: "Total Profit: $4000"
