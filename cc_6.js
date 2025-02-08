a51056a878180448ba42745ea5f589b23bf7766b
mallikanicole <malmichael23@gmail.com>
2025-02-07 20:02:57 -0500

    Task 2-Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,13 @@ profit(costPrice, sellingPrice, unitsSold){
 
 console.log(`Profit:$${calculateProfit(20, 30, 100)}`); // Expected output: "Total Profit: $1000"
 console.log(`Profit:$${calculateProfit(50, 70, 200)}`); // Expected output: "Total Profit: $4000"
+
+
+//Task 2-Function Expression
+let calculateSalesTax=function
+salesTax(amount, taxRate){
+    return(amount*taxRate)
+};//create sales tax function
+
+console.log(`Sales Tax:$${calculateSalesTax(100,0.07)}`); // Expected output: "Sales Tax: $7"
+console.log(`Sales Tax:$${calculateSalesTax(500,0.1)}`);  // Expected output: "Sales Tax: $50"
