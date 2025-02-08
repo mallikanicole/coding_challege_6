d44150dd17a925d13f7a6dc2cb0a23ac82b5dbdc
mallikanicole <malmichael23@gmail.com>
2025-02-07 20:39:47 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -17,3 +17,13 @@ salesTax(amount, taxRate){
 
 console.log(`Sales Tax:$${calculateSalesTax(100,0.07)}`); // Expected output: "Sales Tax: $7"
 console.log(`Sales Tax:$${calculateSalesTax(500,0.1)}`);  // Expected output: "Sales Tax: $50"
+
+
+//Task 3-Arrow Function
+const calculateBonus=(salary,performanceRating)=>
+    salary*performanceRating;//create bonus function
+
+let performanceRating=[Excellent=.2,Good=.1,Average=.05] ;//declare performance rating bonuses
+
+console.log(`Bonus:$${calculateBonus(5000, Excellent)}`); // Expected output: "Bonus: $1000"
+console.log(`Bonus:$${calculateBonus(7000, Good)}`);      // Expected output: "Bonus: $700"
